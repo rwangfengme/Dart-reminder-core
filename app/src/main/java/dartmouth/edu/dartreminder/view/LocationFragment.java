fe1a54de621d238505a3312ecd5bc38e37047077
renjiezhu2 <renjiezhuxianggang@gmail.com>
2016-02-29 18:16:32 -0500

    2/29

--- app/src/main/java/dartmouth/edu/dartreminder/view/LocationFragment.java
+++ app/src/main/java/dartmouth/edu/dartreminder/view/LocationFragment.java
@@ -26,8 +26,6 @@ public class LocationFragment extends Fragment {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_location, container, false);
 
-        
-
         return view;
     }
 
