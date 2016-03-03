ea0d3cd9fd83bf5a1247e07e6d39c6b44a7b81af
renjiezhu2 <renjiezhuxianggang@gmail.com>
2016-03-03 15:43:03 -0500

    Map Activity Add Save and Cancel Button

--- app/src/main/java/dartmouth/edu/dartreminder/utils/Globals.java
+++ app/src/main/java/dartmouth/edu/dartreminder/utils/Globals.java
@@ -11,4 +11,12 @@ public abstract class Globals {
     public static final String[] REPEAT = {"Never", "Every Day", "Every Week", "Every Month", "Every Year"};
     public static final int[] LOCATION_LIST = {R.drawable.ic_add_location_black_48dp, R.drawable.arraw_icon, R.drawable.arraw_icon,
                                             R.drawable.arraw_icon, R.drawable.arraw_icon, R.drawable.arraw_icon};
+
+    public static final String SAVE = "save";
+    public static final String LOCATION_NAME = "location_name";
+    public static final String ARRIVE = "arrive";
+    public static final String RADIUS = "radius";
+    public static final String LAT = "lat";
+    public static final String LNG = "lng";
+    public static final String DEFAULT_LOCATION_NAME = "User Selected Location";
 }
